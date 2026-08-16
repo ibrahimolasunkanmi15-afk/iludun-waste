1fb09c24126ab1087a1c0caeb635d8228d1a9d97
ibrahimolasunkanmi15-afk <ibrahimolasunkanmi15@gmail.com>
2026-08-16 22:05:22 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
+})
